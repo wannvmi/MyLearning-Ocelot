fad9d242630018f657cf619383d48b2684822a74
pursue <996198546@qq.com>
2020-03-30 09:02:19 +0800

    add swagger

--- MyLearning.Ocelot/Demo.Api/Startup.cs
+++ MyLearning.Ocelot/Demo.Api/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Demo.Data;
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Demo.Api
 {
@@ -41,6 +44,10 @@ namespace Demo.Api
             });
 
             services.AddControllers();
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+            });
 
             #region Cors
 
@@ -72,6 +79,26 @@ namespace Demo.Api
             }
 
             #endregion
+
+            #region Swagger
+            //注册Swagger生成器，定义一个和多个Swagger 文档
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Breath.Monitor.API",
+                    Version = "v1",
+                });
+
+                // 设置SWAGER JSON和UI的注释路径。
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Demo.Core.xml"));
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Demo.Data.xml"));
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+
+                // enable swagger Annotations
+                options.EnableAnnotations();
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,6 +108,19 @@ namespace Demo.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            #region Swagger
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("v1/swagger.json", "全部 API");
+                options.RoutePrefix = "swagger";//路径配置，设置为空，表示直接访问该文件，
+                //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件,注意localhost:8001/swagger是访问不到的，
+                //这个时候去launchSettings.json中把"launchUrl": "swagger/index.html"去掉， 然后直接访问localhost:8001/index.html即可
+            });
+            #endregion
 
             app.UsePathBase("/api1");
             app.UseCors();
--- MyLearning.Ocelot/Demo.Ocelot/Startup.cs
+++ MyLearning.Ocelot/Demo.Ocelot/Startup.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -35,7 +39,27 @@ namespace Demo.Ocelot
             services.AddControllersWithViews();
 
             services.AddOcelot(Configuration);
-                //.AddConsul();
+            //.AddConsul();
+
+            #region Swagger
+            //注册Swagger生成器，定义一个和多个Swagger 文档
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Breath.Monitor.API",
+                    Version = "v1",
+                });
+
+                // 设置SWAGER JSON和UI的注释路径。
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Demo.Core.xml"));
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Demo.Data.xml"));
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+
+                // enable swagger Annotations
+                options.EnableAnnotations();
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +69,19 @@ namespace Demo.Ocelot
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            #region Swagger
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("v1/swagger.json", "全部 API");
+                options.RoutePrefix = "swagger";//路径配置，设置为空，表示直接访问该文件，
+                //路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件,注意localhost:8001/swagger是访问不到的，
+                //这个时候去launchSettings.json中把"launchUrl": "swagger/index.html"去掉， 然后直接访问localhost:8001/index.html即可
+            });
+            #endregion
 
             app.UseHttpsRedirection();
 
